2761546777771bd90b48b21578051d2ca10f7b8c
David Madueke <davidmadueke@gmail.com>
2023-08-07 23:26:00 +0100

    Created and Positioned Placeholder Components

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,12 @@
 SimpleMBCompAudioProcessorEditor::SimpleMBCompAudioProcessorEditor (SimpleMBCompAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    addAndMakeVisible(controlBar);
+    addAndMakeVisible(analyzer);
+    addAndMakeVisible(globalControls);
+    addAndMakeVisible(bandControls);
+    
+    setSize (600, 500);
 }
 
 SimpleMBCompAudioProcessorEditor::~SimpleMBCompAudioProcessorEditor()
@@ -35,6 +38,13 @@ void SimpleMBCompAudioProcessorEditor::paint (juce::Graphics& g)
 
 void SimpleMBCompAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+    
+    controlBar.setBounds(bounds.removeFromTop(32));
+    bandControls.setBounds(bounds.removeFromBottom(135));
+    
+    analyzer.setBounds(bounds.removeFromTop(225));
+    
+    globalControls.setBounds(bounds);
+    
 }
